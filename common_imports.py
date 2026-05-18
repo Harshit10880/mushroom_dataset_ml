8e6a00a5acc3553bc0afcfb41bad80ee1e40e7e6
Harshit10880 <harshit.cha10@gmail.com>
2026-05-18 15:20:53 +0530

    updated python file

---
+++ common_imports.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.model_selection import train_test_split
--- python.py
+++ python.py
@@ -1,12 +1,5 @@
 # EDA ( exploratory data analysis )
-import pandas as pd
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_selection import mutual_info_classif
-from sklearn.model_selection import train_test_split
-
+from common_imports import *
 
 
 df = pd.read_csv("mushrooms.csv")
